0e72c0c9d79d43e0743f7792cf8bda234dfc27b6
Vladyslav Diachenko <141845213+aiphae@users.noreply.github.com>
2024-01-10 14:01:49 +0000

    completed change.c

---
+++ course-1/week-3/change.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int change(int money);
+
+int main(void) {
+    int money;
+    scanf("%d", &money);
+    printf("%d\n", change(money));
+
+}
+
+int change(int money) {
+    int count = 0;
+    while (money > 0) {
+        if (money - 10 > 0) {
+            count++;
+            money -= 10;
+        }
+        else if (money - 5 > 0) {
+            count++;
+            money -= 5;
+        }
+        else {
+            count++;
+            money--;
+        }
+    }
+
+    return count;
+}
