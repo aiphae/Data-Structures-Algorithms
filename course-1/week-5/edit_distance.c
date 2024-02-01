bcf50246b4e18e30c55526b09f1cd48e52873d0d
Vladyslav Diachenko <141845213+aiphae@users.noreply.github.com>
2024-02-01 21:24:51 +0000

    fixed edit_distance.c

--- course-1/week-5/edit_distance.c
+++ course-1/week-5/edit_distance.c
@@ -1,38 +1,35 @@
 #include <stdio.h>
 #include <string.h>
 
-int edit_distance(char *str1, char *str2);
+int edit_distance(char str1[], char str2[]);
 int min(int a, int b, int c);
 
 int main(void) {
     char str1[100], str2[100];
-    fgets(str1, 100, stdin);
-    fgets(str2, 100, stdin);
-    str1[strcspn(str1, "\n")] = 0;
-    str2[strcspn(str2, "\n")] = 0;
+    scanf("%s", str1);
+    fflush(stdin);
+    scanf("%s", str2);
     
     printf("%d\n", edit_distance(str1, str2));
 }
 
-int edit_distance(char *str1, char *str2) {
+int edit_distance(char str1[], char str2[]) {
     int length1 = strlen(str1), length2 = strlen(str2);
     int matrix[length1 + 1][length2 + 1];
+
     memset(matrix, 0, (length1 + 1) * (length2 + 1) * sizeof(int));
+
     for (int i = 0; i < length1 + 1; i++)
         matrix[i][0] = i;
+
     for (int i = 0; i < length2 + 1; i++)
         matrix[0][i] = i;
 
-    int insertion, deletion, mismatch, match;
-    for (int j = 1; j <= length2; j++) {
-        for (int i = 1; i <= length1; i++) {
-            insertion = matrix[i][j - 1] + 1;
-            deletion = matrix[i - 1][j] + 1;
-            match = matrix[i - 1][j - 1];
-            mismatch = matrix[i - 1][j - 1] + 1;
-
-            if (str1[i - 1] == str2[j - 1]) matrix[i][j] = min(insertion, deletion, match);
-            else matrix[i][j] = min(insertion, deletion, mismatch);
+    int substitutionCost;
+    for (int j = 1; j < length2 + 1; j++) {
+        for (int i = 1; i < length1 + 1; i++) {
+            substitutionCost = (str1[i - 1] == str2[j - 1]) ? 0 : 1;
+            matrix[i][j] = min(matrix[i - 1][j] + 1, matrix[i][j - 1] + 1, matrix[i - 1][j - 1] + substitutionCost);
         }
     }
 
