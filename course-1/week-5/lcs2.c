5f6880aae0075bd9c6d81ed2f3aec0200ea8b206
Vladyslav Diachenko <141845213+aiphae@users.noreply.github.com>
2024-02-01 20:45:39 +0000

    completed lcs2.c

--- course-1/week-5/lcs2.c
+++ course-1/week-5/lcs2.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 
-int lcs(int seq1[], int seq2[]);
+int lcs(int seq1[], int length1, int seq2[], int length2);
+int max(int a, int b);
 
 int main(void) {
     int length1;
@@ -15,9 +17,23 @@ int main(void) {
     for (int i = 0; i < length2; i++)
         scanf("%d", &seq2[i]);
 
-    printf("%d\n", lcs(seq1, seq2));
+    printf("%d\n", lcs(seq1, length1, seq2, length2));
 }
 
-int lcs(int seq1[], int seq2[]) {
-    
+int lcs(int seq1[], int length1, int seq2[], int length2) {
+    int matrix[length1 + 1][length2 + 1];
+    memset(matrix, 0, (length1 + 1) * (length2 + 1) * sizeof(int));
+
+    for (int i = length1 - 1; i > -1; i--) {
+        for (int j = length2 - 1; j > -1; j--) {
+            if (seq1[i] == seq2[j]) matrix[i][j] = 1 + matrix[i + 1][j + 1];
+            else matrix[i][j] = max(matrix[i][j + 1], matrix[i + 1][j]);
+        }
+    }
+
+    return matrix[0][0];
+}
+
+int max(int a, int b) {
+    return (a > b) ? a : b;
 }
