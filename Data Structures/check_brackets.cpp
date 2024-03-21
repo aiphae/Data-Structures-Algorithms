79c9e21b8ac05ef6fee27a5bd959f4d6e5470db8
Vladyslav Diachenko <141845213+aiphae@users.noreply.github.com>
2024-03-21 11:32:26 +0000

    completed check_brackets.cpp

---
+++ Data Structures/check_brackets.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+struct Bracket {
+    Bracket(char type, int position): type(type), position(position) {}
+
+    bool Matchc(char c) {
+        if (type == '[' && c == ']')
+            return true;
+        if (type == '{' && c == '}')
+            return true;
+        if (type == '(' && c == ')')
+            return true;
+        return false;
+    }
+
+    char type;
+    int position;
+};
+
+int main() {
+    std::string text;
+    getline(std::cin, text);
+
+    std::stack <Bracket> opening_brackets_stack;
+    int position;
+    for (position = 0; position < text.length(); ++position) {
+        char next = text[position];
+
+        if (next == '(' || next == '[' || next == '{') {
+            opening_brackets_stack.push({next, position});
+        }
+
+        else if (next == ')' || next == ']' || next == '}') {
+            if (opening_brackets_stack.empty() || !opening_brackets_stack.top().Matchc(next)) {
+                std::cout << position + 1 << "\n";
+                return 0;
+            }
+            opening_brackets_stack.pop();
+        }
+    }
+
+    if (opening_brackets_stack.empty()) std::cout << "Success\n";
+    else std::cout << opening_brackets_stack.top().position + 1 << "\n"; 
+
+    return 0;
+}
