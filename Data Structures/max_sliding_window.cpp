166f878be0f0b14371017ba3660fe812230b0a03
Vladyslav Diachenko <141845213+aiphae@users.noreply.github.com>
2024-03-21 17:00:50 +0000

    completed max_sliding_window.cpp

--- Data Structures/max_sliding_window.cpp
+++ Data Structures/max_sliding_window.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
+#include <deque>
+#include <stack>
 #include <vector>
 
 using std::cin;
 using std::cout;
 using std::vector;
+using std::deque;
 using std::max;
 
 void max_sliding_window_naive(vector<int> const & A, int w) {
-    for (size_t i = 0; i < A.size() - w + 1; ++i) {
-        int window_max = A.at(i);
-        for (size_t j = i + 1; j < i + w; ++j)
-            window_max = max(window_max, A.at(j));
+    deque<int> window;
 
-        cout << window_max << " ";
+    int i;
+    for (i = 0; i < w; i++) {
+        while (!window.empty() && A[i] >= A[window.back()]) window.pop_back();
+        window.push_back(i);
     }
 
-    return;
-}
+    for (int i = w; i < A.size(); ++i) {
+        cout << A[window.front()] << " ";
 
+        while (!window.empty() && window.front() <= i - w)
+            window.pop_front();
+
+        while (!window.empty() && A[i] >= A[window.back()])
+            window.pop_back();
+
+        window.push_back(i);
+    }
+
+    cout << A[window.front()] << " ";
+}
 
 int main() {
     int n = 0;
